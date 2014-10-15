1df6c84334c776c3f4fdcc835b7a50bf2e20d722
jianglibo <jianglibo@gmail.com>
2014-10-15 21:04:05 +0800

    jdbc driver

--- src/docker_m3958/core.clj
+++ src/docker_m3958/core.clj
@@ -5,3 +5,16 @@
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+;; Apache Derby
+;[org.apache.derby/derby "10.8.1.2"]
+;; HSQLDB
+;[hsqldb/hsqldb "1.8.0.10"]
+;; Microsoft SQL Server using the jTDS driver
+;[net.sourceforge.jtds/jtds "1.2.4"]
+;; MySQL
+;[mysql/mysql-connector-java "5.1.25"]
+;; PostgreSQL
+;[postgresql/postgresql "8.4-702.jdbc4"]
+;; SQLite
+;[org.xerial/sqlite-jdbc "3.7.2"]
