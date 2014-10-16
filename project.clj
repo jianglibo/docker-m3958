c816ed16322880894363f14fc6805eaafe4081a8
jianglibo <jianglibo@gmail.com>
2014-10-16 17:47:49 +0800

    start sql

--- project.clj
+++ project.clj
@@ -3,7 +3,11 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/data.json "0.2.5"]
+                 [org.clojure/java.jdbc "0.3.5"]
+                 [com.mchange/c3p0 "0.9.2.1"]
+                 [org.hsqldb/hsqldb "2.3.2"]]
   :main ^:skip-aot docker-m3958.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/docker_m3958/core.clj
+++ src/docker_m3958/core.clj
@@ -1,11 +1,164 @@
 (ns docker-m3958.core
-  (:gen-class))
+  (:require [clojure.java.io :as io])
+  (:require [clojure.data.json :as json])
+  (:require [clojure.string :as str])
+  (:require [clojure.java.shell :as shell])
+  (:require [clojure.java.jdbc :as j])
+  (:import java.nio.file.Paths)
+  (:import org.hsqldb.jdbc.JDBCDriver)
+  (:import com.mchange.v2.c3p0.ComboPooledDataSource))
+
+(:gen-class)
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (loop [iline nil]
+    (if (or (= iline "exit") (= iline "quit"))
+      (println "bye!")
+      (do (println iline) (recur (read-line))))))
 
+;(take-while str/blank? (repeatedly (read-line)))
+;(take-while (comp str/blank? not) ["1" "2" "" "3"])
+
+(def db-filename (if (nil? (re-find #"(?i)windows" (System/getProperty "os.name")))
+                  "/opt/db/hsqldb"
+                  "c:/db/hsqldb"))
+
+
+(io/make-parents db-filename)
+
+(def db-spec
+  {:classname "org.hsqldb.jdbc.JDBCDriver"
+   :subprotocol "hsqldb"
+   :subname (str "file:" db-filename)
+   :user "aUserName"
+   :password "3xLVz"})
+
+(defn pool
+  [spec]
+  (let [cpds (doto (ComboPooledDataSource.)
+               (.setDriverClass (:classname spec))
+               (.setJdbcUrl (str "jdbc:" (:subprotocol spec) ":" (:subname spec)))
+               (.setUser (:user spec))
+               (.setPassword (:password spec))
+               ;; expire excess connections after 30 minutes of inactivity:
+               (.setMaxIdleTimeExcessConnections (* 30 60))
+               ;; expire connections after 3 hours of inactivity:
+               (.setMaxIdleTime (* 3 60 60)))]
+    {:datasource cpds}))
+
+(def pooled-db (delay (pool db-spec)))
+
+(defn db-connection [] @pooled-db)
+
+
+(j/db-do-commands (db-connection)
+  (j/create-table-ddl :fruit
+                    [:name "varchar(32)" :primary :key]
+                    [:appearance "varchar(32)"]
+                    [:cost :int]
+                    [:grade :real]
+;                    :table-spec "ENGINE=InnoDB"
+                    :entities clojure.string/upper-case))
+
+(j/query (db-connection) ["SELECT * FROM FRUIT"])
+
+(map :table_name
+(j/with-db-metadata [md (db-connection)]
+  (j/metadata-result (.getTables md nil nil nil (into-array ["TABLE"])))))
+
+
+;(j/execute! db-spec
+;            ["update fruit set cost = ( 2 * grade ) where grade > ?" 50.0])
+
+;(j/update! db-spec :fruit
+;           {:cost 49}
+;           ["grade < ?" 75])
+
+;(j/execute! db-spec ["DELETE FROM fruit WHERE grade < ?" 25.0])
+
+;(j/insert! db-spec :fruit
+;           [:name :cost]
+;           ["Mango" 722]
+;           ["Feijoa" 441])
+
+;(j/insert! db-spec :fruit
+;           {:name "Pomegranate" :appearance "fresh" :cost 585}
+;           {:name "Kiwifruit" :grade 93})
+
+;(j/insert! db-spec :fruit {:name "Pear" :appearance "green" :cost 99})
+;(j/drop-table-ddl :fruit :entities clojure.string/upper-case)
+
+
+;ResultSet getTables(String catalog,
+;                    String schemaPattern,
+;                    String tableNamePattern,
+;                    String[] types)
+;                    throws SQLException
+
+;(metadata-result rs-or-value & {:keys [identifiers as-arrays? row-fn result-set-fn], :or {identifiers lower-case, row-fn identity}})
+
+;(doto (io/file db-filename) (.exists))
+;(-> (io/file db-filename) (.exists))
+
+;(with-db-connection [db-con db-spec]
+;  (let [;; fetch some rows using this connection
+;        rows (jdbc/query db-con ["SELECT * FROM table WHERE id = ?" 42])]
+;    ;; insert a copy of the first row using the same connection
+;    (jdbc/insert! db-con :table (dissoc (first rows) :id))))
+
+;(def db-spec
+;  {:classname "com.mysql.jdbc.Driver"
+;   :subprotocol "mysql"
+;   :subname "//127.0.0.1:3306/mydb"
+;   :user "myaccount"
+;   :password "secret"})
+
+;(defn update-or-insert!
+;  "Updates columns or inserts a new row in the specified table"
+;  [db table row where-clause]
+;  (j/with-db-transaction [t-con db]
+;    (let [result (j/update! t-con table row where-clause)]
+;      (if (zero? (first result))
+;        (j/insert! t-con table row)
+;        result))))
+;
+;(update-or-insert! mysql-db :fruit
+;                   {:name "Cactus" :appearance "Spiky" :cost 2000}
+;                   ["name = ?" "Cactus"])
+;;; inserts Cactus (assuming none exists)
+;(update-or-insert! mysql-db :fruit
+;                   {:name "Cactus" :appearance "Spiky" :cost 2500}
+;                   ["name = ?" "Cactus"])
+;;; updates the Cactus we just inserted
+
+;(jdbc/db-do-commands db-spec
+;                     (jdbc/create-table-ddl :fruit
+;                                            [:name "varchar(32)"]
+;                                            [:appearance "varchar(32)"]
+;                                            [:cost :int]
+;                                            [:grade :real]))
+;(jdbc/db-do-commands db-spec "CREATE INDEX name_ix ON fruit ( name )")
+
+;(jdbc/insert! db-spec :table {:col1 42 :col2 "123"}) ;; Create
+;(jdbc/query!  db-spec ["SELECT * FROM table WHERE id = ?" 13]) ;; Read
+;(jdbc/update! db-spec :table {:col1 77 :col2 "456"} ["id = ?" 13]) ;; Update
+;(jdbc/delete! db-spec :table ["id = ?" 13]) ;; Delete
+
+;; (?d) Unix lines (only match \newline)
+;; (?i) Case-insensitive
+;; (?u) Unicode-aware Case
+;; (?m) Multiline
+;; (?s) Dot matches all (including newline)
+;; (?x) Ignore Whitespace and comments
+
+
+;(defn read-from-user
+;  "read user input"
+;  (while (not (str/blank? (read-line)))
+;    )
+;  )
 ;; Apache Derby
 ;[org.apache.derby/derby "10.8.1.2"]
 ;; HSQLDB
@@ -18,3 +171,6 @@
 ;[postgresql/postgresql "8.4-702.jdbc4"]
 ;; SQLite
 ;[org.xerial/sqlite-jdbc "3.7.2"]
+
+;(j/query db-spec ["SELECT * FROM mixedTable"]
+;         :identifiers #(.replace % \_ \-))
